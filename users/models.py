b5e855e1a7a47eccc55a4f4e06286f94f9af7eaf
Arabboydeve <arabboyyunusov2@gmail.com>
2024-06-04 17:23:12 +0500

    models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_img/', blank=True, null=True, default='default_img/user_img.png')
+
+    class Meta:
+        db_table = 'user'
+
+    def __str__(self):
+        return self.username
